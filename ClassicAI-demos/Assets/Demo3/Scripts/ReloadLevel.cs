04e03dd3040c087642cfa0fa3a1e7fc1f3cffc21
Pablo Sánchez <59802775+PabloSanchezTrujillo@users.noreply.github.com>
2020-12-25 19:28:34 +0100

    Reload scene button

---
+++ ClassicAI-demos/Assets/Demo3/Scripts/ReloadLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadLevel : MonoBehaviour
+{
+    public void Reload()
+    {
+        SceneManager.LoadScene((int)Scenes.Scene.Demo3_LevelGenerator, LoadSceneMode.Single);
+    }
+}
---
+++ ClassicAI-demos/Assets/MainMenu/Scripts/Scenes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenes : MonoBehaviour
+{
+    public enum Scene
+    {
+        Demo1_Sekele,
+        Demo1_Demon,
+        Demo1_Hole,
+        Demo1_Angel,
+        Demo2_Combat,
+        Demo3_LevelGenerator
+    }
+}
